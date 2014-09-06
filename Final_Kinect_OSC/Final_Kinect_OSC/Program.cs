ad89540de758bc44aa8dd977174ff2e1aa503400
Jesus Hernandez Tapia <jesus.hernandez.tapia@schneider-electric.com>
2014-09-06 09:02:46 -0700

    Initial Commit - OSC message receiver and transmitter

---
+++ Final_Kinect_OSC/Final_Kinect_OSC/Program.cs
@@ -0,0 +1,120 @@
+﻿namespace kinectTracker
+{
+    using System;
+    using System.Linq;
+    using Microsoft.Kinect;
+    using Microsoft.Kinect.Toolkit;
+    using Microsoft.Kinect.Toolkit.FaceTracking;
+    using System.Threading;
+    using System.Net;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using Bespoke.Common.Osc;
+    using Transmitter;
+
+    internal class Program
+    {
+        public static readonly int Port = 5103;
+
+        private static void Main(string[] args)
+        {
+
+            // Find the first connected sensor
+            KinectSensor sensor = KinectSensor.KinectSensors.Where(s => s.Status == KinectStatus.Connected).FirstOrDefault();
+            if (sensor == null)
+            {
+                Console.WriteLine("No Kinect sensor found!");
+                Console.ReadLine();
+                return;
+            }
+
+            // Create object that will track skeletons using the sensor
+            Tracker tracker = new Tracker(sensor);
+            Console.WriteLine("Tracker created");
+
+            // Start the sensor
+            sensor.Start();
+            Console.WriteLine("Sensor started");
+
+            // Run until the user presses 'q' or 'Q' on the keyboard
+            while (Char.ToLowerInvariant(Console.ReadKey().KeyChar) != 'q') {}
+
+            // Stop the sensor
+            sensor.Stop();
+            kinectTracker.Tracker.transmitter.Stop();
+        }
+
+        public static OscBundle CreateBundle(float X, float Y, float Z)
+        {
+            IPEndPoint sourceEndPoint = new IPEndPoint(IPAddress.Loopback, Port);
+            OscBundle bundle = new OscBundle(sourceEndPoint);
+
+            OscMessage message = new OscMessage(sourceEndPoint, "/handright/");
+            message.Append(X);
+            message.Append(Y);
+            message.Append(Z);
+            bundle.Append(message);
+
+            return bundle;
+        }
+
+    }
+
+    
+
+    internal class Tracker
+    {
+        private Skeleton[] skeletons = null;
+        public static ITransmitter transmitter = new UdpTransmitter();
+
+        public Tracker(KinectSensor sensor)
+        {
+            // Connect the skeleton frame handler and enable skeleton tracking
+            sensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+            sensor.SkeletonFrameReady += SensorSkeletonFrameReady;
+            sensor.SkeletonStream.Enable();
+            Console.WriteLine("Inside tracker");
+        }
+
+        private void SensorSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            // Access the skeleton frame
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+            {
+                //Console.WriteLine("Get in position.");
+                //Console.ReadLine();
+                if (skeletonFrame != null)
+                {
+                    if (this.skeletons == null)
+                    {
+                        // Allocate array of skeletons
+                        this.skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                        Console.WriteLine("Got a skeleton.");
+                    }
+
+                    // Copy skeletons from this frame
+                    skeletonFrame.CopySkeletonDataTo(this.skeletons);
+
+                    // Find first tracked skeleton, if any
+                    Skeleton skeleton = this.skeletons.Where(s => s.TrackingState == SkeletonTrackingState.Tracked).FirstOrDefault();
+
+                    if (skeleton != null)
+                    {
+                        // Obtain the left knee joint; if tracked, print its position
+                        Joint j = skeleton.Joints[JointType.HandRight];
+                        
+
+                        if (j.TrackingState == JointTrackingState.Tracked)
+                        {
+                            Console.WriteLine("Left hand: " + j.Position.X + ", " + j.Position.Y + ", " + j.Position.Z);
+                            
+                            OscBundle bundle = kinectTracker.Program.CreateBundle(j.Position.X, j.Position.Y, j.Position.Z);
+                            transmitter.Start(bundle);
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Max Patches/maxudpsendreceive.maxpat
@@ -0,0 +1,151 @@
+{
+	"patcher" : 	{
+		"fileversion" : 1,
+		"appversion" : 		{
+			"major" : 6,
+			"minor" : 1,
+			"revision" : 7,
+			"architecture" : "x64"
+		}
+,
+		"rect" : [ 66.0, 108.0, 640.0, 480.0 ],
+		"bglocked" : 0,
+		"openinpresentation" : 0,
+		"default_fontsize" : 12.0,
+		"default_fontface" : 0,
+		"default_fontname" : "Arial",
+		"gridonopen" : 0,
+		"gridsize" : [ 15.0, 15.0 ],
+		"gridsnaponopen" : 0,
+		"statusbarvisible" : 2,
+		"toolbarvisible" : 1,
+		"boxanimatetime" : 200,
+		"imprint" : 0,
+		"enablehscroll" : 1,
+		"enablevscroll" : 1,
+		"devicewidth" : 0.0,
+		"description" : "",
+		"digest" : "",
+		"tags" : "",
+		"boxes" : [ 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-8",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 124.0, 135.0, 34.0, 20.0 ],
+					"text" : "print"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-7",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 124.0, 96.0, 99.0, 20.0 ],
+					"text" : "udpreceive 5100"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-6",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 320.0, 206.0, 77.0, 18.0 ],
+					"text" : "/tester/ 1 2 3"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-4",
+					"maxclass" : "toggle",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "int" ],
+					"parameter_enable" : 0,
+					"patching_rect" : [ 320.0, 109.0, 30.0, 30.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-2",
+					"maxclass" : "newobj",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "bang" ],
+					"patching_rect" : [ 320.0, 170.0, 71.0, 20.0 ],
+					"text" : "metro 2000"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-1",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 320.0, 240.0, 140.0, 20.0 ],
+					"text" : "udpsend 127.0.0.1 5100"
+				}
+
+			}
+ ],
+		"lines" : [ 			{
+				"patchline" : 				{
+					"destination" : [ "obj-6", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-2", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-2", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-4", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-1", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-6", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-8", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-7", 0 ]
+				}
+
+			}
+ ],
+		"dependency_cache" : [  ]
+	}
+
+}
