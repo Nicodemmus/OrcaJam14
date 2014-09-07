8880b52eb485e442deac56b813a42584542da9e5
Mateus M F Mendonca <furquim.axs@gmail.com>
2014-09-07 16:42:33 -0700

    Scaling white score

--- XnaBasics/XnaBasics/XnaBasicsGame.cs
+++ XnaBasics/XnaBasics/XnaBasicsGame.cs
@@ -518,7 +518,7 @@ namespace Microsoft.Samples.Kinect.XnaBasics
             this.spriteBatch.Begin();
             this.spriteBatch.Draw(this.header, Vector2.Zero, null, Color.White);
             this.spriteBatch.Draw(this.snare, this.snarePosition, null, Color.White, 0f, Vector2.Zero, snareScale, SpriteEffects.None, 0f);
-            this.spriteBatch.DrawString(this.font, score.ToString() + " (Max: " + maxScore.ToString() + ")", new Vector2(50, 375), Color.Black);
+            this.spriteBatch.DrawString(this.font, score.ToString() + " (Max: " + maxScore.ToString() + ")", new Vector2(50, 350), Color.White, 0.0f, Vector2.Zero, 2.0f, SpriteEffects.None, 0.0f);
             this.spriteBatch.DrawString(this.font, "Hit the drums and maintain the rythm.", new Vector2(100, this.viewPortRectangle.Y + this.viewPortRectangle.Height + 3), Color.Black);
             this.spriteBatch.End();
 
